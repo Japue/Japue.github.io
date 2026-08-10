dfc3b1416c6dfcfb5388a7c82c7bae390477b08b
Jasper Rochus <jasper.rochus@gmail.com>
2026-08-10 14:37:04 +0200

    added functionality

--- HTML-projects/pokemon-finder/index.js
+++ HTML-projects/pokemon-finder/index.js
@@ -1,3 +1,21 @@
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then(response => console.log(response))
-    .catch(error => console.error(error));
+const textBox = document.getElementById("text-box");
+const image = document.getElementById("pokemon-sprite");
+
+
+
+function fetchData(){
+    const search = textBox.value.toLowerCase();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+        .then(response => {
+            if (!response.ok) {
+                image.style.display = "none";
+                throw new Error(`Pokemon ${search} not found`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            image.style.display = "block";
+            image.src = data.sprites.front_default;
+        })
+        .catch(error => console.error(error));
+}
