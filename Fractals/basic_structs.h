2bb03d82cab3e789549167f53a39363306aae4cb
Jasper Rochus <jasper.rochus@gmail.com>
2026-08-15 11:39:12 +0200

    added default constructors to structs

--- Fractals/basic_structs.h
+++ Fractals/basic_structs.h
@@ -4,6 +4,7 @@ struct Point {
     float x;
     float y;
 
+    Point() : x(0), y(0) {}
     Point(float x_, float y_) : x(x_), y(y_) {}
     Point(int x_, int y_) : x(static_cast<float>(x_)), y(static_cast<float>(y_)) {}
 };
@@ -13,6 +14,8 @@ struct Line {
     Point end;
     float length;
 
+    Line() : begin(Point()), end(Point()), length(0) {}
+
     Line(Point begin_, Point end_, float length_)
-        : begin(begin_), end(end_), length(length_) {};
+        : begin(begin_), end(end_), length(length_) {}
 };
